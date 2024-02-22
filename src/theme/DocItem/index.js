380b3e950065801a4b17d5b241a529cbe50ad96c
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-02-22 20:13:30 +0800

    fix

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -27,6 +27,7 @@ export default function DocItemWrapper(props) {
             reactionsEnabled="0"
             emitMetadata="0"
             inputPosition="top"
+            defaultCommentOrder="newest"
             theme="transparent_dark"
             lang="zh-CN"
             loading="lazy"
