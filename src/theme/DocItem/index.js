9417e195a81df1b47a00607ca23ce6a91032855b
dsx137 <1419696375@qq.com>
2024-02-22 20:01:54 +0800

    fix

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -13,27 +13,27 @@ export default function DocItemWrapper(props) {
       <DocItem {...props} />
       {comments && (
         <div>
-        <Giscus
-          id="comments"
-          // highlight-warn-start
-          repo="Async-Lab/Asyncraft"
-          repoId="R_kgDOJjWTSg"
-          category="Comments"
-          categoryId="DIC_kwDOJjWTSs4CdYHH"
-          // highlight-warn-end
-          mapping="title"
-          strict="1"
-          term="Welcome to @giscus/react component!"
-          reactionsEnabled="0"
-          emitMetadata="0"
-          inputPosition="bottom"
-          theme="transparent_dark"
-          lang="zh-CN"
-          loading="lazy"
-        />
-      </div>
+          <Giscus
+            id="comments"
+            // highlight-warn-start
+            repo="Async-Lab/Asyncraft"
+            repoId="R_kgDOJjWTSg"
+            category="Comments"
+            categoryId="DIC_kwDOJjWTSs4CdYHH"
+            // highlight-warn-end
+            mapping="title"
+            strict="1"
+            term="Welcome to @giscus/react component!"
+            reactionsEnabled="0"
+            emitMetadata="0"
+            inputPosition="top"
+            theme="transparent_dark"
+            lang="zh-CN"
+            loading="lazy"
+          />
+        </div>
       )}
-      
+
     </>
   );
 }
