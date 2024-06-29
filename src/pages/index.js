15c411e224ce4064c55961b446cb29a431f74240
dsx137 <70027572+dsx137@users.noreply.github.com>
2024-06-30 04:34:33 +0800

    update and background

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,13 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import { useEffect, useState } from 'react';
 
 import styles from './index.module.css';
+
+const transitionDuration = 2000;
+const stayDuration = 10000;
+const maxOpacity = 0.25
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -20,6 +25,61 @@ function HomepageHeader() {
   );
 }
 
+const BackgroundImageComponent = () => {
+  const [backgroundImage, setBackgroundImage] = useState('');
+  const [opacity, setOpacity] = useState(0);
+
+  const imageContext = require.context('../../static/img/背景', false, /\.(png|jpe?g|svg)$/);
+
+  const fetchImages = async () => {
+    try {
+      const images = imageContext.keys();
+      const randomImage = images[Math.floor(Math.random() * images.length)];
+      if (backgroundImage === `/img/背景/${randomImage}` && images.length > 1) {
+        fetchImages();
+        return;
+      }
+      setBackgroundImage(`/img/背景/${randomImage}`);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+    setOpacity(maxOpacity);
+
+    const interval = setInterval(() => {
+      setOpacity(0);
+
+      setTimeout(() => {
+        fetchImages();
+        setOpacity(maxOpacity);
+      }, transitionDuration);
+    }, transitionDuration + stayDuration);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div id='background-image'
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        position: 'fixed',
+        zIndex: -999,
+        width: '100%',
+        height: '100%',
+        backgroundSize: 'auto',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        filter: `blur(1.5px) opacity(${opacity})`,
+        transition: `filter ${transitionDuration}ms ease`,
+      }}
+    />
+  );
+};
+
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -31,16 +91,15 @@ export default function Home() {
         <center>
           <h1 className="sub__title">Java版/基岩版互通</h1>
           <div>
-            <img className='qq' src="/img/qrcode-dark.png" width="300px" style={{ margin: "0 30px" }}></img>
-            {/* <iframe className='kook' src="https://kookapp.cn/widget?id=5570057485746728&theme=dark" width="300" height="374.47" allowtransparency="true" frameborder="0"></iframe> */}
+            <img className='qq' src="/img/qrcode-dark.png" width="300px" style={{ margin: "0 30px" }} />
             <a href="https://kook.top/ZpHV6s">
-              <img src="/img/kook.png" width="300px" style={{ margin: "0 30px" }}>
-              </img>
+              <img src="/img/kook.png" width="300px" style={{ margin: "0 30px" }} />
             </a>
           </div>
-          <div style={{height:"200px"}}></div>
+          <div style={{ height: "200px" }}></div>
         </center>
       </main>
+      <BackgroundImageComponent />
     </ Layout>
   );
 }
