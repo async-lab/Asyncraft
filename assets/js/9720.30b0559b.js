c3b1b28d1ea87421da77eb1db9d7e9830edf4fbe
dsx137 <dsx137@users.noreply.github.com>
2024-11-10 15:14:01 +0000

    deploy: 539583f53bc676c0a69ed38adc7c18b151e59277

---
+++ assets/js/9720.30b0559b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkasyncraft=self.webpackChunkasyncraft||[]).push([[9720],{9720:(e,c,a)=>{a.d(c,{createArchitectureServices:()=>r.S});var r=a(9936);a(9369)}}]);
--- js/UrlFormatter.js
+++ js/UrlFormatter.js
@@ -1,1 +1,16 @@
-function formatURL(){var t=window.location.pathname;"/"===t||t.endsWith("/")||history.replaceState({},"",t+"/")}setInterval(formatURL,100);
+// 在所有页面URL后面添加斜杠("/")，以避免重定向
+
+function formatURL() {
+    // 获取当前路径
+    var currentPath = window.location.pathname;
+
+    // 如果不是根页面且不以斜杠("/")结尾
+    if (currentPath !== "/" && !currentPath.endsWith("/")) {
+        // 使用history.replaceState更改地址栏URL，但不重新加载页面
+        history.replaceState({}, "", currentPath + "/");
+    }
+};
+
+
+
+setInterval(formatURL, 100);
